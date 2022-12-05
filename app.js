6c9d4156c1ef1177cd90232731bcebcea7b6105b
Puja-3033 <19203033@iubat.edu>
2022-12-05 19:55:39 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+let menu = document.querySelector('nav ul')
+let hamburger = document.querySelector('nav .mobile')
+
+hamburger.addEventListener('click', function()
+{
+    menu.classList.toggle('show')
+}
+)
